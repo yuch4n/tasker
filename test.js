86b505ad3d3fa98f1716c84e7c22ae3b3514e896
yuch4n <yuch4n@gmail.com>
2020-10-18 22:53:35 +0900

    Update test.js

--- test.js
+++ test.js
@@ -3,4 +3,4 @@ document.getElementsByTagName('body')[0].insertAdjacentHTML('afterbegin','test')
 let body = document.createElement('body');
 body.insertAdjacentHTML('afterbegin','test');
 */
-flash(document.getElementsByTagName('body')[0]);
+flash(document.getElementsByTagName('html')[0]);
