dc9ad15650284856204094fdef5c7aa28a40a511
yuch4n <yuch4n@gmail.com>
2020-10-18 21:36:33 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,3 @@
-document.getElementsByTagName('body')[0].insertAdjacentHTML('afterbegin','test');
+//document.getElementsByTagName('body')[0].insertAdjacentHTML('afterbegin','test');
+let body = document.createElement('body');
+body.insertAdjacentHTML('afterbegin','test');
